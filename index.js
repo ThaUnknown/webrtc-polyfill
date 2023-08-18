d9f5f6a08da8da7b4321af82e7a75298583f7f5b
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2023-08-18 14:21:34 +0200

    feat: initial implementation

---
+++ index.js
@@ -0,0 +1,7 @@
+export { default as RTCPeerConnection } from './lib/RTCPeerConnection.js'
+export { default as RTCSessionDescription } from './lib/RTCSessionDescription.js'
+export { default as RTCIceCandidate } from './lib/RTCIceCandidate.js'
+export { RTCDataChannelEvent, RTCPeerConnectionIceEvent } from './lib/Events.js'
+export { RTCError, RTCErrorEvent } from './lib/RTCError.js'
+
+export * as default from './index.js'
