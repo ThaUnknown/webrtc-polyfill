ab057fe5ab30c69a8e6c02c4f1bdbce01edc638a
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2023-09-16 01:08:17 +0200

    chore: update NDC

--- test/constants.js
+++ test/constants.js
@@ -1,17 +1,4 @@
 export const EXCLUSIONS = [
-  // navigator.getUserMedia
-  // 'receiver-track-live.https.html',
-  // 'recvonly-transceiver-can-become-sendrecv.https.html',
-  // 'RollbackEvents.https.html',
-  // 'RTCPeerConnection-add-track-no-deadlock.https.html',
-  // 'RTCIceConnectionState-candidate-pair.https.html',
-  // 'RTCDTMFSender-ontonechange-long.https.html',
-  // 'RTCPeerConnection-connectionState.https.html',
-  // MediaStream
-  // 'RTCPeerConnection-capture-video.https.html',
-  // 'RTCDtlsTransport-state.html',
-  // AudioContext
-  // 'RTCDtlsTransport-getRemoteCertificates.html',
   // utility files
   'RTCCertificate-postMessage.html',
   // getConfig/setConfig which is not implemented
@@ -22,11 +9,12 @@ export const EXCLUSIONS = [
   'RTCPeerConnection-generateCertificate.html',
   // RTCCertificate which is not implemented
   'RTCCertificate.html',
-  // mysterious process exits
-  'RTCPeerConnection-addIceCandidate.html',
-  'RTCPeerConnection-canTrickleIceCandidates.html',
-  'RTCPeerConnection-createDataChannel.html',
-  'RTCPeerConnection-getStats.https.html'
+  // RTCDTMFSender which is not implemented
+  'RTCDTMFSender-insertDTMF.https.html',
+  'RTCDTMFSender-ontonechange-long.https.html',
+  'RTCDTMFSender-ontonechange.https.html',
+  // canvas
+  'RTCPeerConnection-relay-canvas.https.html'
 ]
 
 export const WPT_SERVER_URL = 'http://web-platform.test:8000'
